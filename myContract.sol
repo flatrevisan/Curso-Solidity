585e411c43e0b89dd1a0faee48da1632eaade7ec
Flavia Trevisan <67347200+flatrevisan@users.noreply.github.com>
2020-07-06 21:53:38 -0300

    Update myContract.sol
    Conditionals

--- myContract.sol
+++ myContract.sol
@@ -83,4 +83,59 @@ contract MyContract
         return value;
     }   
     
+//SPDX-License-Identifier: CC-BY-4.0
+pragma solidity 0.6.10;
+
+contract MyContract 
+{
+    
+//Conditionals
+// if and else
+    uint [] public numbers = [1,2,3,4,5,6,7,8,9,10];
+    address public owner;
+    
+    constructor () public 
+    {
+        owner = msg.sender;
+    }
+
+    function countEvenNumbers () public view returns (uint)
+    {
+        uint count = 0;
+        
+        for (uint i = 0; i <numbers.length; i++) // runs code until the conditional is met
+        {
+            if (isEvenNumber(numbers[i]))
+            {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    function isEvenNumber(uint _number) public view returns(bool) 
+    {
+        if (_number % 2 == 0)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isOwner () public view returns (bool)
+    {
+        if (msg.sender == owner)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function wasOwner () public view returns (bool)
+    {
+        return (msg.sender == owner);
+    }
+}
     
